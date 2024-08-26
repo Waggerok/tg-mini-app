a57f9d56b6bc9fd3b190607ccf79728d8199c943
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-08-26 14:03:01 +0300

    initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const tg = window.Telegram.WebApp;
 
 const App = () => {
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    const onClose = () => {
+        tg.close();
+    }
+
     return (
         <>
-            
+            work
+
+            <button onClick={onClose}>Закрыть приложение</button>
         </>
     )
 }
