80d86a38a963703ccc884f191b94259648178c44
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-09-07 11:48:58 +0300

    added card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import React, { useEffect } from 'react';
 //Icons
 import { FaApple } from "react-icons/fa";
 
+//Components
+import ProductCard from './components/ProductCard';
+
+
 const tg = window.Telegram.WebApp;
 
 const App = () => {
@@ -13,13 +17,24 @@ const App = () => {
 
     return (
         <div className="App">
-            <section className='shop'>
-                <div className="shop__title">
+            <section className='welcome'>
+                <div className="welcome__title">
                     <FaApple size={40}/>
                     Apple Store
                 </div>
-                <div className="shop__description">
+                <div className="welcome__description">
                     Добро пожаловать в наш магазин техники Apple Store, мы переехали в Telegram Mini Apps, теперь вы можете заказывать вашу любимую продукцию прямо из Telegram.
+                </div>
+            </section>
+            <section className="shop">
+                <div className="shop__title">
+                    Наши товары:
+                </div>
+                <div className="shop__description">
+                    У нас большое разнообразие товаров, начиная от Airpods и iPhone, заканчивая Macbook.
+                </div>
+                <div className="shop__products">
+                    <ProductCard/>
                 </div>
             </section>
         </div>
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import airpods from '../Products/Airpods';
+
+const ProductCard  = () => {
+    return (
+        <>
+            <div className="card">
+                <div className="card__img"></div>
+                <div className="card__title"></div>
+                <div className="card__price"></div>
+            </div>
+        </>
+    );
+};
+
+export default ProductCard;
